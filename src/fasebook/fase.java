8f79c42a3907b1909998af5948a1d1e9c043729a
sojood97 <35595423+sojood97@users.noreply.github.com>
2018-04-03 13:30:08 +0300

    fasebook commit

---
+++ src/fasebook/fase.java
@@ -0,0 +1,24 @@
+package fasebook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
+public class fase {
+	static WebDriver driver;
+	public static void main (String []ar)
+	{
+		System.setProperty("WebDriver.gecko.driver", "C:\\Users\\Soso\\Downloads\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("sojood.alsayyed@gmail.com");
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("0597800333soso");
+		Select sel =new Select(driver.findElement(By.xpath(".//*[@id='month']")));
+		sel.selectByIndex(6);
+		driver.findElement(By.xpath(".//*[@id='u_0_v']")).click();
+	}
+
+}
